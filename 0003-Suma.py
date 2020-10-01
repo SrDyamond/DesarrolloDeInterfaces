42ba3c3e7e02af43dd2b191fc5b75f10bbfa2514
Dyango Sanchez <dsanchezr@fpcoruna.afundacion.org>
2020-10-01 18:46:15 +0200

    ExperimentosCasa

---
+++ 0003-Suma.py
@@ -0,0 +1,17 @@
+#Suma
+numero1=1
+numero2=3
+suma=0
+print("El numero 1 es:")
+print(numero1)
+print("El numero 2 es:")
+print(numero2)
+
+suma=numero1+numero2
+print("La suma es:")
+print(suma)
+
+if suma>5:
+    print("La suma es mayor de 5")
+else:
+    print("La suma es menor de 5")
