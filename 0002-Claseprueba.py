7085d2fe01f7ee4abf8d77fdbee271f62a9e8840
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-10-01 14:36:13 +0200

    add PrimerosProgramas

---
+++ 0002-Claseprueba.py
@@ -0,0 +1,15 @@
+# Aqui defino mi clase
+class Congratulador:
+	def felicitar(self,nombre1,nombre2):
+		print("Felicidades" +nombre1,nombre2)
+		
+##	def saludar(self)
+##		print("Lo que quiera")
+
+#Aqui comienda a ejecutarse
+#mi programa
+
+
+#instanciamos
+a = Congratulador()
+a.felicitar("Pepe","Juan")
