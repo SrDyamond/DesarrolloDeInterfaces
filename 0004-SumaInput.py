3655c0bbc42210da71b33f83d52b5571adbb577f
Dyango Sanchez <dsanchezr@fpcoruna.afundacion.org>
2020-10-01 19:26:23 +0200

    ExperimentosPythonCasa

---
+++ 0004-SumaInput.py
@@ -0,0 +1,9 @@
+#SumaInput
+numero1 = input("Inserte o primeiro numero: ")
+numero2 = input(f"Inserte o segundo numero: ")
+numero1=int(numero1)
+numero2=int(numero2)
+#Convirto os numero a int,porque o pedilos por teclado
+#gardanse como str
+suma=numero1+numero2
+print(suma)
---
+++ 0005-RestaInput.py
@@ -0,0 +1,6 @@
+#RestaInput
+numero1 = int(input("Digame un numero: "))
+#Covirto directamente o numero1
+#en int cando o pido por teclado
+numero2 = int(input(f"Digame un numero mayor que {numero1}: "))
+print(f"La diferencia entre ellos es {numero2 - numero1}.")
