42c92eb2ce2a642e359d9eda2a3fdf7664bd80b8
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-10-05 12:16:39 +0200

    #2: Tarea 2. Interfaz para contraseñas

---
+++ #Spring1/password.py
@@ -0,0 +1,32 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class PasswordWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Contraseña")
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+        self.entry = Gtk.Entry()
+        self.entry.set_text("Introduzca su contraseña")
+        vbox.pack_start(self.entry, True, True, 0)
+
+        button = Gtk.Button(label="Aceptar")
+        button.connect("clicked", self.on_button_clicked)
+        vbox.pack_start(button, True, True, 0)
+
+    def on_button_clicked(self, widget):
+        if self.entry.get_text() == "pass":
+            print("Correcta. Cerrando ventana...")
+            Gtk.main_quit()
+        else:
+            print("Incorrecta")
+
+window = PasswordWindow()
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+
+Gtk.main()
