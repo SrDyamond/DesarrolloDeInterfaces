f0690eb20f399c20f4e2fd4af70ee74eb01b67a2
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-10-13 11:48:24 +0200

    Update

--- #Sprint1/#4interfaz.py
+++ #Sprint1/#4interfaz.py
@@ -1,22 +1,28 @@
 import gi
-
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Producto")
-        boxprincipal = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=30)
+        boxprincipal = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=30)
+        self.add(boxprincipal)
 
-        abox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=30)
+        abox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
         boxprincipal.add(abox)
+
+        bbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
+        boxprincipal.add(bbox)
 
         image = Gtk.Image()
         image.set_from_file("nombre.PNG")
         abox.pack_start(image, True, True, 50)
 
-        bbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=30)
-        boxprincipal.add(bbox)
+        image = Gtk.Image()
+        image.set_from_file("tarjeta.PNG")
+        bbox.pack_start(image, True, True, 50)
+
+    
 
 
 win = MyWindow()
