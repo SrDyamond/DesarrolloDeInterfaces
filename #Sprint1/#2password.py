727b40de3618e6d47103d9e0a7b7aa55b40dbb5f
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-10-13 09:11:32 +0200

    Actualizacion #2password.py con varias clases en varios archivos

--- #Sprint1/#2password.py
+++ #Sprint1/#2password.py
@@ -1,48 +1,33 @@
 import gi
-
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-from password_fail.py import PasswordFail
-from password_ok.py import PasswordOk
+
+from ok_window import OkWindow
+from fail_window import FailWindow
 
 class PasswordWindow(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Contraseña")
+        Gtk.Window.__init__(self, title="Pass")
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.add(box)
 
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
+        label = Gtk.Label("Introduce tu contraseña")
+        label.set_padding(50,50)
+        box.pack_start(label, True, True, 0)
 
         self.entry = Gtk.Entry()
-        self.entry.set_text("Introduzca su contraseña")
-        vbox.pack_start(self.entry, True, True, 0)
-
-        hbox = Gtk.Box(spacing=6)
-        vbox.pack_start(hbox, True, True, 0)
-
-        self.check_visible = Gtk.CheckButton(label="Visible")
-        self.check_visible.connect("toggled", self.on_visible_toggled)
-        self.check_visible.set_active(True)
-        hbox.pack_start(self.check_visible, True, True, 0)
-
-
+        box.pack_start(self.entry, True, True, 0)
         button = Gtk.Button(label="Aceptar")
         button.connect("clicked", self.on_button_clicked)
-        vbox.pack_start(button, True, True, 0)
-
-    def on_visible_toggled(self, button):
-        value = button.get_active()
-        self.entry.set_visibility(value)
+        box.pack_start(button, True, True, 0)
 
     def on_button_clicked(self, widget):
         if self.entry.get_text() == "pass":
-            window = PasswordOk()
+            window = OkWindow()
             window.show_all()
-            Gtk.main()
         else:
-            window = PasswordFail()
+            window = FailWindow()
             window.show_all()
-
-            Gtk.main()
 
 window = PasswordWindow()
 window.connect("destroy", Gtk.main_quit)
