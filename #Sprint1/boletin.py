562b30b0f512ed2c05b686f6bc67adbf773e85ed
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-10-19 12:12:51 +0200

    Tarea opcional.Ejercicio 1.1 1.2

---
+++ #Sprint1/boletin.py
@@ -0,0 +1,11 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+from ventana import Ventana
+
+window = Ventana()
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+
+Gtk.main()
