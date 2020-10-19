688807238c4c799926c88a43d3f33b51b60fc66b
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-10-19 13:11:25 +0200

    Añadido codigo y captura 4.2

--- #Sprint1/ventana.py
+++ #Sprint1/ventana.py
@@ -6,6 +6,6 @@ class Ventana(Gtk.Window):
 	def __init__(self):
 		Gtk.Window.__init__(self)
 		self.set_position(Gtk.WindowPosition.MOUSE)
-		label = Gtk.Label(label = "Texto")
+		label = Gtk.Label(label = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum")
 		label.set_size_request(300, 150)
 		self.add(label)
