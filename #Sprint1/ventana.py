dfd4c2b495c91d49e4d89b143ba1ab7600ce49ce
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-10-19 13:43:52 +0200

    Añadido codigo,y captura 5.1

--- #Sprint1/ventana.py
+++ #Sprint1/ventana.py
@@ -6,9 +6,12 @@ class Ventana(Gtk.Window):
 	def __init__(self):
 		Gtk.Window.__init__(self)
 		self.set_position(Gtk.WindowPosition.MOUSE)
+		box = Gtk.Box()
+		self.add(box)
+
 		label = Gtk.Label(label = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum")
 		label.set_line_wrap(True)
 		label.set_justify(Gtk.Justification.FILL)
 		label.set_max_width_chars(50)
 		label.set_size_request(300, 150)
-		self.add(label)
+		box.pack_start(label, True, True, 0)
