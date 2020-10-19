ea80a77367b5b5f8ac8453d6158c9b8a417574bd
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-10-19 13:10:02 +0200

    Añadido codigo y captura 4.1

--- #Sprint1/ventana.py
+++ #Sprint1/ventana.py
@@ -6,16 +6,6 @@ class Ventana(Gtk.Window):
 	def __init__(self):
 		Gtk.Window.__init__(self)
 		self.set_position(Gtk.WindowPosition.MOUSE)
-		box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-		self.add(box)
-
-		button = Gtk.CheckButton(label="Boton Activable")
-		button.set_size_request(300, 150)
-		button.connect("clicked", self.ha_clicado)
-		box.pack_start(button, True, True, 0)
-
-	def ha_clicado(self, widget):
-			if widget.get_active() == True:
-				print("El checkButton ha sido marcado")
-			else:
-				print("El checkButton ha sido desmarcado")
+		label = Gtk.Label(label = "Texto")
+		label.set_size_request(300, 150)
+		self.add(label)
