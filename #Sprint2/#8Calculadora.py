8298350b4c113d98a76341ad7c397899c974330a
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-10-26 13:01:45 +0100

    #8: Tarea 3 (Sprint 2).Codigo calculadora

---
+++ #Sprint2/#8Calculadora.py
@@ -0,0 +1,54 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from calculadora_controlador import Controlador
+
+builder = Gtk.Builder()
+builder.add_from_file("calculadora.glade")
+
+label_resultado = builder.get_object("label_result")
+controlador = Controlador(label_resultado)
+
+boton0 = builder.get_object("button0")
+boton0.connect("clicked", controlador.ha_pulsado_0)
+boton1 = builder.get_object("button1")
+boton1.connect("clicked", controlador.ha_pulsado_1)
+boton2 = builder.get_object("button2")
+boton2.connect("clicked", controlador.ha_pulsado_2)
+boton3 = builder.get_object("button3")
+boton3.connect("clicked", controlador.ha_pulsado_3)
+boton4 = builder.get_object("button4")
+boton4.connect("clicked", controlador.ha_pulsado_4)
+boton5 = builder.get_object("button5")
+boton5.connect("clicked", controlador.ha_pulsado_5)
+boton6 = builder.get_object("button6")
+boton6.connect("clicked", controlador.ha_pulsado_6)
+boton7 = builder.get_object("button7")
+boton7.connect("clicked", controlador.ha_pulsado_7)
+boton8 = builder.get_object("button8")
+boton8.connect("clicked", controlador.ha_pulsado_8)
+boton9 = builder.get_object("button9")
+boton9.connect("clicked", controlador.ha_pulsado_9)
+
+boton_punto = builder.get_object("button_dot")
+boton_punto.connect("clicked", controlador.ha_pulsado_punto)
+boton_borrar = builder.get_object("buttonC")
+boton_borrar.connect("clicked", controlador.ha_pulsado_borrar)
+
+boton_dividir = builder.get_object("button_divide")
+boton_dividir.connect("clicked", controlador.ha_pulsado_dividir)
+boton_multiplicar = builder.get_object("button_multiply")
+boton_multiplicar.connect("clicked", controlador.ha_pulsado_multiplicar)
+boton_sumar = builder.get_object("button_plus")
+boton_sumar.connect("clicked", controlador.ha_pulsado_sumar)
+boton_restar = builder.get_object("button_minus")
+boton_restar.connect("clicked", controlador.ha_pulsado_restar)
+boton_igual = builder.get_object("button_equals")
+boton_igual.connect("clicked", controlador.ha_pulsado_igual)
+
+window = builder.get_object("window")
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+
+Gtk.main()
---
+++ #Sprint2/calculadora_controlador.py
@@ -0,0 +1,149 @@
+class Controlador:
+
+	# Inicializador
+
+	def __init__(self, label_resultado):
+		self.operacion_pulsada = None
+		self.primer_operando = None
+		self.ha_pulsado_operacion_o_igual = False
+		self.label_resultado = label_resultado
+		self.label_resultado.set_text("")
+
+	# Metodos que responden a las acciones del usuario
+
+	# Botones numericos
+
+	def ha_pulsado_0(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "0")
+
+	def ha_pulsado_1(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "1")
+
+	def ha_pulsado_2(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "2")
+
+	def ha_pulsado_3(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "3")
+
+	def ha_pulsado_4(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "4")
+
+	def ha_pulsado_5(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "5")
+
+	def ha_pulsado_6(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "6")
+
+	def ha_pulsado_7(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "7")
+
+	def ha_pulsado_8(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "8")
+
+	def ha_pulsado_9(self, widget):
+		self._limpiar_pantalla_si_es_necesario()
+		texto_actual = self.label_resultado.get_text()
+		self.label_resultado.set_text(texto_actual + "9")
+
+	# Botones de coma decimal y borrado
+
+	def ha_pulsado_punto(self, widget):
+		if self._ha_introducido_punto() == False:
+			# El usuario todavia no ha introducido una coma decimal
+			texto_actual = self.label_resultado.get_text()
+			self.label_resultado.set_text(texto_actual + ".")
+		else:
+			# Ya hay una coma decimal en pantalla.
+			# No hacemos nada. No puede haber un número con 2 comas decimales
+			pass
+
+	def ha_pulsado_borrar(self, widget):
+		self.operacion_pulsada = None
+		self.primer_operando = None
+		self.ha_pulsado_operacion_o_igual = False
+		self.label_resultado.set_text("")
+
+	# Botones de operacion
+
+	def ha_pulsado_sumar(self, widget):
+		self.ha_pulsado_operacion_o_igual = True
+		self._operar_y_mostrar_resultado_si_es_necesario()
+		self.primer_operando = float(self.label_resultado.get_text())
+		self.operacion_pulsada = "+"
+
+	def ha_pulsado_restar(self, widget):
+		self.ha_pulsado_operacion_o_igual = True
+		self._operar_y_mostrar_resultado_si_es_necesario()
+		self.primer_operando = float(self.label_resultado.get_text())
+		self.operacion_pulsada = "-"
+
+	def ha_pulsado_multiplicar(self, widget):
+		self.ha_pulsado_operacion_o_igual = True
+		self._operar_y_mostrar_resultado_si_es_necesario()
+		self.primer_operando = float(self.label_resultado.get_text())
+		self.operacion_pulsada = "*"
+
+	def ha_pulsado_dividir(self, widget):
+		self.ha_pulsado_operacion_o_igual = True
+		self._operar_y_mostrar_resultado_si_es_necesario()
+		self.primer_operando = float(self.label_resultado.get_text())
+		self.operacion_pulsada = "/"
+
+	def ha_pulsado_igual(self, widget):
+		self.ha_pulsado_operacion_o_igual = True
+		self._operar_y_mostrar_resultado_si_es_necesario()
+		self.primer_operando = None
+		self.operacion_pulsada = None
+
+	# Metodos de uso preferiblemente privado
+
+	def _operar_y_mostrar_resultado_si_es_necesario(self):
+		if self.primer_operando is not None:
+			valor_actual = float(self.label_resultado.get_text())
+			resultado = self._efectuar_operacion(self.primer_operando, valor_actual, self.operacion_pulsada)
+			self.label_resultado.set_text(str(round(resultado, 5)))
+
+	def _efectuar_operacion(self, op1, op2, operacion):
+		assert op1 is not None, "Inconsistencia"
+		assert op2 is not None, "Inconsistencia"
+		if operacion == "+":
+			return op1 + op2
+		elif operacion == "-":
+			return op1 - op2
+		elif operacion == "*":
+			return op1 * op2
+		elif operacion == "/":
+			return op1 / op2
+		else:
+			assert False, "Inconsistencia"
+
+	def _ha_introducido_punto(self):
+		try:
+			posicion = self.label_resultado.get_text().index('.')
+			print("Se introdujo una coma decimal, tecleada en la " + posicion + " pulsacion")
+			return True
+		except ValueError:
+			return False
+
+	def _limpiar_pantalla_si_es_necesario(self):
+		if self.ha_pulsado_operacion_o_igual:
+			self.ha_pulsado_operacion_o_igual = False
+			self.label_resultado.set_text("")
