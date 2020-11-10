74c943d87128603f5ce901d55d8429e4c6aab77c
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-11-10 09:42:36 +0100

    #13-Tarea 2. Creación de un programa Python que renderice la interfaz implementada en Glade al final del sprint anterior

--- #Sprint3/catalogo.py
+++ #Sprint3/catalogo.py
@@ -7,26 +7,8 @@ from ventana_reservas import Reservas
 builder = Gtk.Builder()
 builder.add_from_file("catalogo.glade")
 
-
-class Catalogo (Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Catalogo")
-
-        boton_reservas = builder.get_object("Boton_Reservas")
-        boton_reservas.connect("clicked", self.on_button1_clicked)
-
-        boton_pagar = builder.get_object("Boton_Pagar")
-        boton_pagar.connect("clicked", self.on_button2_clicked)
-
-    def on_button1_clicked(self, widget):
-        print("hola")
-
-    def on_button2_clicked(self, widget):
-        print("clic")
-
-        # window = Reservas()
-        # window.show_all()
-
+boton_reservas = builder.get_object("Boton_Reservas")
+boton_reservas.connect("clicked", Reservas.mostrarReservas)
 
 window = builder.get_object("window")
 window.connect("destroy", Gtk.main_quit)
--- #Sprint3/ventana_reservas.py
+++ #Sprint3/ventana_reservas.py
@@ -8,5 +8,8 @@ builder.add_from_file("reservas.glade")
 class Reservas(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="")
+
+    def mostrarReservas(self):
+        print("123")
         window = builder.get_object("window")
-        
+        window.show_all()
