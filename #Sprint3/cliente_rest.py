3644c510b99df5a4e214eba94d58de674bf1b558
Dyango Carlos <dsanchezr@fpcoruna.afundacion.org>
2020-11-12 14:32:49 +0100

    #13-Tarea 3.Update

--- #Sprint3/cliente_rest.py
+++ #Sprint3/cliente_rest.py
@@ -5,6 +5,18 @@ class ClienteRest:
 		response = requests.get('http://localhost:8080/product')
 		return response.json()
 
-    def solicitar_producto(self, referencia):
-    	response = requests.get('http://localhost:8080/product/' + referencia)
-    	return response.json()
+	def solicitar_producto(self, referencia):
+		response = requests.get('http://localhost:8080/product/' + referencia)
+		return response.json()
+
+	def solicitar_reserva(self,referencia,username):
+		response = requests.get("http://localhost:8080/"+username+"/reserve/"+referencia)
+		return response.json()
+
+	def crear_reserva(self,referencia,username):
+		response = requests.post("http://localhost:8080/"+username+"/reserve/"+referencia")
+		return response.json()
+
+	def borrar_reserva(seld,referencia,username):
+		response = requests.delete("http://localhost:8080/"+username+"/reserve/"+referencia")
+		return response.json()
